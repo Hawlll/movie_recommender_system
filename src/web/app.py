9318ac62f4325c6d388bcf0cc5b66ef049cd85f3
Roberto <robertoramirezgalan04@gmail.com>
2025-03-29 17:02:43 -0400

    Finished register.html and finished login screen

--- src/web/app.py
+++ src/web/app.py
@@ -13,5 +13,10 @@ def login():
     return render_template('login_screen.html')
 
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    return render_template('register.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
