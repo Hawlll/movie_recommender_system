3cab4379ce5070c355f63e46ed0be628fa6a44cf
Astermaxed <ajm9738@psu.edu>
2025-03-29 16:29:40 -0400

    Added base results and survey page(which isnt done yet) and updated base code with the survery page

--- src/web/app.py
+++ src/web/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
@@ -8,6 +8,19 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/survey', methods=['GET', 'POST'])
+def survey():
+    if request.method == 'POST':
+        # Get responses from the form
+        name = request.form.get("name")
+        q1 = request.form.get("question1")
+        q2 = request.form.get("question2")
+        q3 = request.form.get("question3")
+
+        # Store or process the data (for now, just displaying)
+        return render_template('results.html', name=name, q1=q1, q2=q2, q3=q3)
+
+    return render_template('survey.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
