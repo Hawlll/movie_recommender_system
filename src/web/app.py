3f7b42146a707354a0ad634a80906ba27d794e2e
Roberto <robertoramirezgalan04@gmail.com>
2025-03-29 20:05:14 -0400

    Get username and password from the input boxes

--- src/web/app.py
+++ src/web/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -10,12 +10,28 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        if username == username and password == password:
+            return "Login Success"
+        else:
+            return "Invalid username or password, please try again"
+
     return render_template('login_screen.html')
 
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template('register.html')
+
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        return f"Account {username} has been created. Please login."
+
+    return render_template("register.html")
 
 
 if __name__ == '__main__':
